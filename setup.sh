b5ac20b4c987228d88fca629b859cb608d4b57ce
Ubuntu <ebartrum@shapenet.majj3fxkpakevfaq5xt5z1susf.zx.internal.cloudapp.net>
2019-02-07 13:08:39 +0000

    Make directories for config files. Symlink fish key bindings.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+mkdir -p ~/.config/fish/functions
+mkdir -p ~/.config/nvim
 ln -is ~/Documents/dot-files/.tmux.conf ~/.tmux.conf
 ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
+ln -is ~/Documents/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 ln -is ~/Documents/dot-files/init.vim ~/.config/nvim/init.vim
