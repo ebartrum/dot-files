016caf470e61130c51c889569606c557b2b3b96c
Ed Bartrum <edward.bartrum@gmail.com>
2019-02-11 16:56:54 +0000

    Include i3 config

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
 ln -is ~/Documents/dot-files/config-extra.fish ~/.config/fish/config-extra.fish
 ln -is ~/Documents/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 ln -is ~/Documents/dot-files/init.vim ~/.config/nvim/init.vim
+ln -is ~/Documents/dot-files/i3-config ~/.config/i3/config
