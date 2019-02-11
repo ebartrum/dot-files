a754573cdf63c03c4fd5aa92b3630b838ac304a2
Ed Bartrum <edward.bartrum@gmail.com>
2019-02-11 13:08:23 +0000

    Create env_vars.fish in setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mkdir -p ~/.config/fish/functions
 mkdir -p ~/.config/nvim
+touch env_vars.fish
 ln -is ~/Documents/dot-files/.tmux.conf ~/.tmux.conf
 ln -is ~/Documents/dot-files/.tmux-ssh.conf ~/.tmux-ssh.conf
 ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
+ln -is ~/Documents/dot-files/env_vars.fish ~/.config/fish/env_vars.fish
 ln -is ~/Documents/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 ln -is ~/Documents/dot-files/init.vim ~/.config/nvim/init.vim
