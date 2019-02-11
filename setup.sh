fd61e0c896bb071a4a324e95667d42389249a0b8
Ed Bartrum <edward.bartrum@gmail.com>
2019-02-11 16:14:34 +0000

    Typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.config/fish/functions
 mkdir -p ~/.config/nvim
-touch env_vars.fish
+touch env-vars.fish
 ln -is ~/Documents/dot-files/.tmux.conf ~/.tmux.conf
 ln -is ~/Documents/dot-files/.tmux-ssh.conf ~/.tmux-ssh.conf
 ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
