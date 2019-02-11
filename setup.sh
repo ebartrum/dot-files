c9d378ac9d024d3d5a5d24cead1a893402166c6f
Ed Bartrum <edward.bartrum@gmail.com>
2019-02-11 13:12:47 +0000

    Fix typo

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ touch env_vars.fish
 ln -is ~/Documents/dot-files/.tmux.conf ~/.tmux.conf
 ln -is ~/Documents/dot-files/.tmux-ssh.conf ~/.tmux-ssh.conf
 ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
-ln -is ~/Documents/dot-files/env_vars.fish ~/.config/fish/env_vars.fish
+ln -is ~/Documents/dot-files/env-vars.fish ~/.config/fish/env-vars.fish
 ln -is ~/Documents/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 ln -is ~/Documents/dot-files/init.vim ~/.config/nvim/init.vim
