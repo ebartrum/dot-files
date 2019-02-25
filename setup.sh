532ea1f790e4b13bf3ec73dbdd0fa63b3aa97434
Edward Bartrum <edward.bartrum@gmail.com>
2019-02-25 21:51:03 +0000

    i3status-config

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 mkdir -p ~/.config/fish/functions
 mkdir -p ~/.config/nvim
 touch config-extra.fish
-ln -is ~/Documents/dot-files/.Xresources ~/.Xresources
 ln -is ~/Documents/dot-files/.tmux.conf ~/.tmux.conf
 ln -is ~/Documents/dot-files/.tmux-ssh.conf ~/.tmux-ssh.conf
 ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
@@ -10,4 +9,5 @@ ln -is ~/Documents/dot-files/config-extra.fish ~/.config/fish/config-extra.fish
 ln -is ~/Documents/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 ln -is ~/Documents/dot-files/init.vim ~/.config/nvim/init.vim
 ln -is ~/Documents/dot-files/i3-config ~/.config/i3/config
+ln -is ~/Documents/dot-files/i3status-config ~/.config/i3status/config
 ln -is ~/Documents/dot-files/.Xdefaults ~/.Xdefaults
