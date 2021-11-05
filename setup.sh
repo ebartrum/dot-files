9df3632ecbc8afb5c4294e197706ea3c4168ab61
Ed Bartrum <edward.bartrum@gmail.com>
2021-11-05 12:09:41 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,12 +5,10 @@ mkdir -p ~/.config/i3status
 mkdir -p ~/.config/i3
 
 touch config-extra.fish
-ln -is ~/Documents/dot-files/.tmux.conf ~/.tmux.conf
-ln -is ~/Documents/dot-files/.tmux-ssh.conf ~/.tmux-ssh.conf
-ln -is ~/Documents/dot-files/config.fish ~/.config/fish/config.fish
-ln -is ~/Documents/dot-files/config-extra.fish ~/.config/fish/config-extra.fish
-ln -is ~/Documents/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
-ln -is ~/Documents/dot-files/init.vim ~/.config/nvim/init.vim
-ln -is ~/Documents/dot-files/i3-config ~/.config/i3/config
-ln -is ~/Documents/dot-files/i3status-config ~/.config/i3status/config
-ln -is ~/Documents/dot-files/.Xdefaults ~/.Xdefaults
+ln -is ~/Documents/repos/dot-files/config.fish ~/.config/fish/config.fish
+ln -is ~/Documents/repos/dot-files/config-extra.fish ~/.config/fish/config-extra.fish
+ln -is ~/Documents/repos/dot-files/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
+ln -is ~/Documents/repos/dot-files/init.vim ~/.config/nvim/init.vim
+ln -is ~/Documents/repos/dot-files/i3-config ~/.config/i3/config
+ln -is ~/Documents/repos/dot-files/i3status-config ~/.config/i3status/config
+ln -is ~/Documents/repos/dot-files/.Xdefaults ~/.Xdefaults
