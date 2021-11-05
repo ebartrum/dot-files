92bc64b2d232558468cc9c32c6fc79199b88f542
Ed Bartrum <edward.bartrum@gmail.com>
2021-11-05 12:07:13 +0000

    Keybinding to search and edit

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -1,9 +1,10 @@
 function fish_user_key_bindings
   bind \co forward-word
   bind \ck up-or-search
-  bind \cp accept-autosuggestion 
+  bind \cp accept-autosuggestion
   bind \cq kill-whole-line
   bind \ch backward-kill-word
+  bind \e\cO search_and_edit
   bind \c_ 'prevd >/dev/null; commandline -f repaint'
   bind \c] 'nextd >/dev/null; commandline -f repaint'
   fzf_key_bindings
