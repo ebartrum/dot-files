03388321ab7a9f4236db051b5f4d941b67433f17
Edward Bartrum <edward.bartrum@gmail.com>
2024-04-12 16:10:56 +0900

    Add kuja xmonad cfg

---
+++ xmonad/kuja.hs
@@ -0,0 +1,41 @@
+import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Util.Dmenu
+import XMonad.Util.SpawnOnce
+import XMonad.Util.Run
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import System.Exit
+import Control.Monad
+
+quitWithWarning :: X ()
+quitWithWarning = do
+    let m = "confirm quit"
+    s <- dmenu [m]
+    when (m == s) (io exitSuccess)
+
+myStartupHook = do
+    spawnOnce "feh --bg-scale ~/Documents/repos/wallpapers/images/low_poly_landscape.png &"
+    spawnOnce "picom --config ~/.config/picom.conf &"
+
+lockScreen = do
+    spawn "betterlockscreen -l &"
+
+mySB = statusBarProp "xmobar" (pure xmobarPP)
+
+main :: IO ()
+main = xmonad $ ewmhFullscreen $ ewmh $ withEasySB mySB defToggleStrutsKey myConfig
+
+myConfig = def
+        { terminal    = "alacritty"
+        , modMask     = mod4Mask
+        , borderWidth = 4
+        , focusedBorderColor = "#138029"
+        , startupHook = myStartupHook
+        }
+  `additionalKeysP`
+	    [("M-S-q", quitWithWarning)
+	    ,("M-S-l", lockScreen)
+	    ,("M-f", spawn "firefox")
+	    ]
