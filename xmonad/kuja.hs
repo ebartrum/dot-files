690399fe7c8342fefec9274715babbcbd7bb6b78
Edward Bartrum <edward.bartrum@gmail.com>
2024-08-09 13:57:01 +0900

    Change xmonad's multimonitor behaviour on kuja

--- xmonad/kuja.hs
+++ xmonad/kuja.hs
@@ -6,6 +6,7 @@ import XMonad.Util.Run
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+import qualified XMonad.StackSet as W
 import System.Exit
 import Control.Monad
 
@@ -32,10 +33,17 @@ mySB = statusBarProp "xmobar" (pure xmobarPP)
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ withEasySB mySB defToggleStrutsKey myConfig
 
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
 myKeys = [("M-S-q", quitWithWarning)
     ,("M-S-l", lockScreen)
     ,("M-S-s", screenshotScreen)
     ,("M-f", spawn "firefox")
+    ] ++ 
+    [ (otherModMasks ++ "M-" ++ [key], action tag)
+      | (tag, key)  <- zip myWorkspaces "123456789"
+      , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
+                                      , ("S-", windows . W.shift)]
     ]
 
 myConfig = def
