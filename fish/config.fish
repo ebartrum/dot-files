ab674769df651f375fbc69079dec4d348b071d74
Edward Bartrum <edward.bartrum@gmail.com>
2025-04-07 14:08:27 +0000

    Fish config

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+#Remove greeting
+set --erase fish_greeting
+
+#Abbreviations
+abbr --add gch git checkout
+abbr --add gchb git checkout -b
+abbr --add gchp git checkout -p
+abbr --add gcm git commit -m
+abbr --add gca git commit --amend
+abbr --add gstsh git stash
+abbr --add gd git diff
+abbr --add gl git log
+abbr --add gla git log --all
+abbr --add ga git add
+abbr --add gap git add -p
+abbr --add gb git branch
+abbr --add gbd git branch -d
+abbr --add gs git status
