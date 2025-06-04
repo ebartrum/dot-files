8cf3bcc69e82e4a80add0a7239f2c4e5617dda12
Edward Bartrum <edward.bartrum@gmail.com>
2025-06-04 12:12:48 +0000

    Add fish config for docker images

---
+++ fish/docker_config.fish
@@ -0,0 +1,32 @@
+#Remove greeting
+set -g fish_greeting
+
+#Abbreviations
+abbr --add gch git checkout
+abbr --add gchb git checkout -b
+abbr --add gchp git checkout -p
+abbr --add gcm git commit -m
+abbr --add gca git commit --amend
+abbr --add gstsh git stash
+abbr --add gd git diff
+abbr --add gl git log
+abbr --add gla git log --all
+abbr --add ga git add
+abbr --add gap git add -p
+abbr --add gb git branch
+abbr --add gbd git branch -d
+abbr --add gs git status
+abbr --add vim nvim
+
+#key-bindings
+bind \co forward-word
+bind \ck up-or-search
+bind \cp accept-autosuggestion
+
+#Environment variables
+set -gx EDITOR nvim
+
+#prompt
+function fish_right_prompt -d "Write out the right prompt"
+    echo (set_color yellow) $STY (set_color normal)
+end
